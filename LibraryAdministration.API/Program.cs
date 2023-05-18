1712ba2f7a647c65868cf695d779c6cf03c74b0f
Razvan <razvan.neamtu96@gmail.com>
2023-05-19 01:38:56 +0300

    added swagger configs for authentication

--- LibraryAdministration.API/Program.cs
+++ LibraryAdministration.API/Program.cs
@@ -3,6 +3,7 @@ using LibraryAdministration.DataAccess.Context;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace LibraryAdministration.API
@@ -23,37 +24,62 @@ namespace LibraryAdministration.API
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Library Administration API", Version = "v1" });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Provide a token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                            .AddJwtBearer(options =>
-                            {
-                                options.TokenValidationParameters = new TokenValidationParameters()
-                                {
-                                    ClockSkew = TimeSpan.Zero,
-                                    ValidateIssuer = true,
-                                    ValidateAudience = true,
-                                    ValidateLifetime = true,
-                                    ValidateIssuerSigningKey = true,
-                                    ValidIssuer = configuration["JWTAuthentication:ValidIssuer"],
-                                    ValidAudience = configuration["JWTAuthentication:ValidAudience"],
-                                    IssuerSigningKey = new SymmetricSecurityKey(
-                                        Encoding.UTF8.GetBytes(configuration["JWTAuthentication:IssuerSigningKey"])
-                                    ),
-                                };
-                            });
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ClockSkew = TimeSpan.Zero,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["JWTAuthentication:ValidIssuer"],
+                        ValidAudience = configuration["JWTAuthentication:ValidAudience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTAuthentication:IssuerSigningKey"])),
+                    };
+                });
 
             builder.Services.AddIdentityCore<IdentityUser>(options =>
-                            {
-                                options.SignIn.RequireConfirmedAccount = false;
-                                options.User.RequireUniqueEmail = true;
-                                options.Password.RequireDigit = false;
-                                options.Password.RequiredLength = 6;
-                                options.Password.RequireNonAlphanumeric = false;
-                                options.Password.RequireUppercase = false;
-                                options.Password.RequireLowercase = false;
-                            })
-                            .AddEntityFrameworkStores<UsersContext>();
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+                .AddEntityFrameworkStores<UsersContext>();
 
             var app = builder.Build();
 
